336faa03fd7663c9864da28ca066cc3ef6ec33ca
Sam Cochrane <Sam@Sams-MacBook-Air.local>
2017-07-16 19:19:35 +1000

    update for twilio

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
 const serviceAccount = require('./service_account.json');
+const requestOneTimePassword = require('./requestOneTimePassword');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -9,4 +10,4 @@ admin.initializeApp({
 });
 
 exports.createUser = functions.https.onRequest(createUser);
-
+exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword);
